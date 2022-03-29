ad33004a3c0ca29ad5dd712c8af38dc45c767346
Stanley Cooper <scooperlu@gmail.com>
2022-03-29 18:20:47 -0400

    Added AddRunButton component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import RunLog from './components/RunLog';
 import Header from './layout/Header';
+import AddRunButton from './components/AddRunButton';
 
 export default function App() {
   return (
     <main className="h-screen bg-gradient-to-b from-gray-500 to-gray-800">
       <Header />
       <RunLog />
+      <div className="flex justify-end px-2 py-2">
+        <AddRunButton />
+      </div>
     </main>
   )
 }
---
+++ src/components/AddRunButton.js
@@ -0,0 +1,9 @@
+function AddRunButton() {
+    return (
+        <div className="bg-slate-900 hover:bg-slate-700 text-white font-bold w-20 py-2 px-4 rounded">
+            <button>Add +</button>
+        </div>
+    )
+}
+
+export default AddRunButton
